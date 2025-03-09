87ee80882ca52ba93f5151790850274a08f9fd08
joker3c3 <youngin9630@gmail.com>
2025-03-09 09:31:04 +0900

    Changed parallaxSpeed

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ let currentScroll = 0;
 const maxScroll = (sections.length - 1) * window.innerWidth;
 const parallaxSpeed = {
   back: 0.1, // 가장 느리게
-  mid: 0.4, // 중간 속도
-  front: 0.7, // 가장 빠르게
+  mid: 0.3, // 중간 속도
+  front: 0.8, // 가장 빠르게
 };
 
 // 스크롤 위치에 따른 배경 이동
